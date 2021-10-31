0f86944147ece3cb8edcb835d5385c7311487cba
Mickey Phillips <phillipsmickey41@gmail.com>
2021-10-30 21:57:06 -0500

    added sessions and login/logout js

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUnintialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
